fd18c026309fbd46aac3831691ccb9195acd6c63
Martina del Pino <martudelpino@gmail.com>
2022-04-26 14:15:43 -0300

    comienzo

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    < BrowserRouter>
+      <div>
+        <Switch>
+          <Route />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/components/PagPrincipal/pagPrincipal.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function pagPrincipal () {
+    return (
+        <div>
+            <h1> Bienvenidos </h1>
+        </div>
+    )
+}
+
+export default pagPrincipal;
